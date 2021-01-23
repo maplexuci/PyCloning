61593fc78a38e1648e63777fc7bb1d6cc22fd729
maplexuci <zhhxu9@gmail.com>
2021-01-22 23:54:56 -0500

    Add menu bar and submenu items

--- pycloning.py
+++ pycloning.py
@@ -11,8 +11,43 @@ root.configure(bg="lightblue")
 root.columnconfigure(0, weight=2)
 root.columnconfigure((1,2), weight=1)
 
-VERSION = "V0.1"
+# Create menu bar
+menubar = Menu(root)
+root.config(menu=menubar)
 
+# Create 'File' menu
+file_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New DNA File...")
+file_menu.add_command(label="New Protein File...")
+file_menu.add_separator()
+file_menu.add_command(label="Open Files...")
+file_menu.add_command(label="Open Recent File")
+file_menu.add_command(label="Close", command=root.quit)
+file_menu.add_separator()
+file_menu.add_command(label="Import")
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+# Create 'Cloning' menu
+cloning_menu = Menu(menubar, tearoff=False)
+menubar.add_cascade(label="Cloning", menu=cloning_menu)
+cloning_menu.add_command(label="PCR...")
+cloning_menu.add_command(label="Overlapping PCR")
+cloning_menu.add_command(label="Mutagenesis...")
+cloning_menu.add_separator()
+cloning_menu.add_command(label="Restriction Enzyme Cloning")
+cloning_menu.add_command(label="Linear Ligation")
+cloning_menu.add_separator()
+cloning_menu.add_command(label="TA or GC Cloning")
+cloning_menu.add_command(label="TOPO® Cloning")
+cloning_menu.add_command(label="Gateway® Cloning")
+cloning_menu.add_command(label="Gibson Assembly®")
+cloning_menu.add_command(label="In-Fusion® Cloning")
+cloning_menu.add_command(label="NEBuilder® HiFi DNA Assembly")
+
+
+VERSION = "V0.1"
 ver_title = Label(root, text="PyCloning " + VERSION, font=("Courier", 24, "bold"), bg="lightblue")
 ver_title.grid(row=0, column=0, columnspan=3, padx=220, pady=10, sticky=E+W)
 
@@ -24,11 +59,11 @@ status = Label(root, bd=1, relief=SUNKEN, height=3)
 status.grid(row=6, column=0, columnspan=3, sticky=W+E)
 
 pixelVirtual = PhotoImage(width=1, height=1)
-btn_ndf = Button(root, image=pixelVirtual, width=40, height=40)
-btn_npf = Button(root, image=pixelVirtual, width=40, height=40)
-btn_open = Button(root, image=pixelVirtual, width=40, height=40)
-btn_orf = Button(root, image=pixelVirtual, width=40, height=40)
-btn_import = Button(root, image=pixelVirtual, width=40, height=40)
+btn_ndf = Button(root, image=pixelVirtual, width=35, height=35)
+btn_npf = Button(root, image=pixelVirtual, width=35, height=35)
+btn_open = Button(root, image=pixelVirtual, width=35, height=35)
+btn_orf = Button(root, image=pixelVirtual, width=35, height=35)
+btn_import = Button(root, image=pixelVirtual, width=35, height=35)
 
 lbl_ndf = Label(root, text="New DNA File...", bg="lightblue")
 lbl_npf = Label(root, text="New Protein File...", bg="lightblue")
@@ -47,8 +82,6 @@ lbl_npf.grid(row=2, column=2, pady=10, sticky=W)
 lbl_open.grid(row=3, column=2, pady=10, sticky=W)
 lbl_orf.grid(row=4, column=2, pady=10, sticky=W)
 lbl_import.grid(row=5, column=2, pady=10, sticky=W)
-
-
 
 
 root.mainloop()
